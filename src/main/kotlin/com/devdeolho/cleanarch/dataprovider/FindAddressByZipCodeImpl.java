64abc4b4de7dc9e278d6d56bf16c7200d55078ab
DionatanLeao <dionatan.ti@outlook.com>
2025-06-27 18:34:31 -0300

    feat: Creating the address lookup dataprovider

---
+++ src/main/kotlin/com/devdeolho/cleanarch/dataprovider/FindAddressByZipCodeImpl.java
@@ -0,0 +1,21 @@
+package com.devdeolho.cleanarch.dataprovider;
+
+import com.devdeolho.cleanarch.core.dataprovider.FindAddressByZipCode;
+import com.devdeolho.cleanarch.core.domain.Address;
+import com.devdeolho.cleanarch.dataprovider.client.FindAddressByZipCodeClient;
+import com.devdeolho.cleanarch.dataprovider.client.mapper.AddressResponseMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class FindAddressByZipCodeImpl implements FindAddressByZipCode {
+
+    @Autowired
+    private FindAddressByZipCodeClient findAddressByZipCodeClient;
+
+    @Autowired
+    private AddressResponseMapper addressResponseMapper;
+
+    @Override
+    public Address find(String zipCode) {
+        return addressResponseMapper.toAddress(findAddressByZipCodeClient.find(zipCode));
+    }
+}
