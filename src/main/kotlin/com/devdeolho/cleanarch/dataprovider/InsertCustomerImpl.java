a643212122d377c4bd65b0d7288e87f0b160f6e1
DionatanLeao <dionatan.ti@outlook.com>
2025-06-28 20:24:51 -0300

    feat: Creating the client insertion dataprovider

---
+++ src/main/kotlin/com/devdeolho/cleanarch/dataprovider/InsertCustomerImpl.java
@@ -0,0 +1,19 @@
+package com.devdeolho.cleanarch.dataprovider;
+
+import com.devdeolho.cleanarch.core.dataprovider.InsertCustomer;
+import com.devdeolho.cleanarch.core.domain.Customer;
+import com.devdeolho.cleanarch.dataprovider.repository.CustomerRepository;
+import com.devdeolho.cleanarch.dataprovider.repository.mapper.CustomerEntityMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class InsertCustomerImpl implements InsertCustomer {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CustomerEntityMapper customerEntityMapper;
+    @Override
+    public void insert(Customer customer) {
+        customerRepository.save(customerEntityMapper.toCustomerEntity(customer));
+    }
+}
