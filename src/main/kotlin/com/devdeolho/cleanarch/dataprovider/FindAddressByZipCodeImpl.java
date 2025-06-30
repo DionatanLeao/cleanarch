e47e702f5bc1ed768a6226ffe001ab2062e417c3
DionatanLeao <dionatan.ti@outlook.com>
2025-06-30 18:32:02 -0300

    feat: Creating the dataprovider

--- src/main/kotlin/com/devdeolho/cleanarch/dataprovider/FindAddressByZipCodeImpl.java
+++ src/main/kotlin/com/devdeolho/cleanarch/dataprovider/FindAddressByZipCodeImpl.java
@@ -5,7 +5,9 @@ import com.devdeolho.cleanarch.core.domain.Address;
 import com.devdeolho.cleanarch.dataprovider.client.FindAddressByZipCodeClient;
 import com.devdeolho.cleanarch.dataprovider.client.mapper.AddressResponseMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FindAddressByZipCodeImpl implements FindAddressByZipCode {
     @Autowired
     private FindAddressByZipCodeClient findAddressByZipCodeClient;
--- src/main/kotlin/com/devdeolho/cleanarch/dataprovider/UpdateCustomerImpl.java
+++ src/main/kotlin/com/devdeolho/cleanarch/dataprovider/UpdateCustomerImpl.java
@@ -1,6 +1,6 @@
 package com.devdeolho.cleanarch.dataprovider;
 
-import com.devdeolho.cleanarch.core.dataprovider.InsertCustomer;
+import com.devdeolho.cleanarch.core.dataprovider.UpdateCustomer;
 import com.devdeolho.cleanarch.core.domain.Customer;
 import com.devdeolho.cleanarch.dataprovider.repository.CustomerRepository;
 import com.devdeolho.cleanarch.dataprovider.repository.mapper.CustomerEntityMapper;
@@ -8,14 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class InsertCustomerImpl implements InsertCustomer {
+public class UpdateCustomerImpl implements UpdateCustomer {
     @Autowired
     private CustomerRepository customerRepository;
 
     @Autowired
     private CustomerEntityMapper customerEntityMapper;
+
     @Override
-    public void insert(Customer customer) {
+    public void update(Customer customer) {
         customerRepository.save(customerEntityMapper.toCustomerEntity(customer));
     }
 }
