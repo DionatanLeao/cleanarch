db5b6ccccc413d7497a4f728afdab5370d4be464
DionatanLeao <dionatan.ti@outlook.com>
2025-07-03 19:57:38 -0300

    feat: Creating Kafka Configurations

---
+++ src/main/kotlin/com/devdeolho/cleanarch/entrypoint/consumer/message/CustomerMessage.java
@@ -0,0 +1,16 @@
+package com.devdeolho.cleanarch.entrypoint.consumer.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerMessage {
+    private String id;
+    private String name;
+    private String cpf;
+    private String zipCode;
+    private Boolean isValidCpf;
+}
