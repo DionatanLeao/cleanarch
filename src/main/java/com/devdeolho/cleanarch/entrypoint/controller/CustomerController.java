c5f04d8f7350eb8f29f644091073ae5e4225580e
DionatanLeao <dionatan.ti@outlook.com>
2026-03-23 18:22:00 -0300

    feat: Creating the entry point

--- src/main/java/com/devdeolho/cleanarch/entrypoint/controller/CustomerController.java
+++ src/main/java/com/devdeolho/cleanarch/entrypoint/controller/CustomerController.java
@@ -1,7 +1,9 @@
 package com.devdeolho.cleanarch.entrypoint.controller;
 
+import com.devdeolho.cleanarch.core.domain.Customer;
 import com.devdeolho.cleanarch.core.usecase.FindCustomerByIdUseCase;
 import com.devdeolho.cleanarch.core.usecase.InsertCustomerUseCase;
+import com.devdeolho.cleanarch.core.usecase.UpdateCustomerUseCase;
 import com.devdeolho.cleanarch.entrypoint.controller.mapper.CustomerMapper;
 import com.devdeolho.cleanarch.entrypoint.controller.request.CustomerRequest;
 import com.devdeolho.cleanarch.entrypoint.controller.response.CustomerResponse;
@@ -11,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +29,9 @@ public class CustomerController {
     private FindCustomerByIdUseCase findCustomerByIdUseCase;
 
     @Autowired
+    private UpdateCustomerUseCase updateCustomerUseCase;
+
+    @Autowired
     private CustomerMapper customerMapper;
 
     @PostMapping
@@ -38,5 +44,14 @@ public class CustomerController {
     @GetMapping("/{id}")
     public ResponseEntity<CustomerResponse> findById(@PathVariable final String id) {
         return ResponseEntity.ok().body(customerMapper.toCustomerResponse(findCustomerByIdUseCase.find(id)));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@RequestBody @Valid CustomerRequest customerRequest,
+                                       @PathVariable final String id) {
+        Customer customer = customerMapper.toCustomer(customerRequest);
+        customer.setId(id);
+        updateCustomerUseCase.update(customer, customerRequest.getZipCode());
+        return ResponseEntity.noContent().build();
     }
 }
