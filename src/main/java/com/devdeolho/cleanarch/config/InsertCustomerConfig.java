5a6165809b27c1f12088d4e34a16c04be3595e32
DionatanLeao <dionatan.ti@outlook.com>
2026-03-29 19:08:00 -0300

    feat: Creating the producer

--- src/main/java/com/devdeolho/cleanarch/config/InsertCustomerConfig.java
+++ src/main/java/com/devdeolho/cleanarch/config/InsertCustomerConfig.java
@@ -3,6 +3,7 @@ package com.devdeolho.cleanarch.config;
 import com.devdeolho.cleanarch.core.usecase.impl.InsertCustomerUseCaseImpl;
 import com.devdeolho.cleanarch.dataprovider.FindAddressByZipCodeImpl;
 import com.devdeolho.cleanarch.dataprovider.InsertCustomerImpl;
+import com.devdeolho.cleanarch.dataprovider.SendCpfForValidationImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,8 +13,10 @@ public class InsertCustomerConfig {
     @Bean
     public InsertCustomerUseCaseImpl insertCustomerUseCase(
             FindAddressByZipCodeImpl findAddressByZipCode,
-            InsertCustomerImpl insertCustomer
+            InsertCustomerImpl insertCustomer,
+            SendCpfForValidationImpl sendCpfForValidation
+
     ) {
-        return new InsertCustomerUseCaseImpl(findAddressByZipCode, insertCustomer);
+        return new InsertCustomerUseCaseImpl(findAddressByZipCode, insertCustomer, sendCpfForValidation);
     }
 }
